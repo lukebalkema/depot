a9958a60acd507bfb1e721abab71b501053b58c3
lukebalkema <144456031+lukebalkema@users.noreply.github.com>
2023-12-11 12:17:05 -0500

    NEWEST

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'admin/index'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  resources :users
   resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
